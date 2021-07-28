5e86a37c87ef64ff83b87facf76edc24d960e9cb
SumanIslam <sumanislam@gmail.com>
2021-07-28 17:59:59 +0600

    refractored some code in TodoList

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,24 +2,27 @@ import List from '@material-ui/core/List';
 import Todo from './Todo';
 import Paper from '@material-ui/core/Paper';
 function TodoList({ todos, removeTodo, toggleCompletion, editTodo }) {
-  return(
-    <Paper elevation={7}>
-      <List>
-        {todos.map(todo => (
-          <Todo
-            key={todo.id} 
-            id={todo.id} 
-            task={todo.task} 
-            completed={todo.completed} 
-            removeTodo={removeTodo} 
-            toggleCompletion={toggleCompletion}
-            editTodo={editTodo}
-          />
-      ))}
-      </List>
-      
-    </Paper>
-  )
+  if(todos.length) {
+    return(
+      <Paper elevation={7}>
+        <List>
+          {todos.map(todo => (
+            <Todo
+              key={todo.id} 
+              id={todo.id} 
+              task={todo.task} 
+              completed={todo.completed} 
+              removeTodo={removeTodo} 
+              toggleCompletion={toggleCompletion}
+              editTodo={editTodo}
+            />
+        ))}
+        </List>
+        
+      </Paper>
+    )
+  }
+  return null;
 }
 
 export default TodoList;
